73c82e0ad3a05614b0cf09701a7be4d20f401939
onionhammer <erik.m.oleary@gmail.com>
2014-05-25 16:26:23 -0500

    Update Entry.cpp

--- Entry.cpp
+++ Entry.cpp
@@ -9,16 +9,16 @@ int main(int argc, char *argv[]) {
     // Allow application to receive joystick events without focus
     SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1");
 
-	// Initialize SDL & TTF Libraries
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK)) std::cerr << SDL_GetError();
-	if (TTF_Init()) std::cerr << SDL_GetError();
+    // Initialize SDL & TTF Libraries
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK)) std::cerr << SDL_GetError();
+    if (TTF_Init()) std::cerr << SDL_GetError();
 
     // Create a Shell
     Shell shell;
 
     //Run Shell
-	shell.Run();
+    shell.Run();
 
-	SDL_Quit();
+    SDL_Quit();
     return 0;
 }
